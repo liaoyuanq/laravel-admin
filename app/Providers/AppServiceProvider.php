6925ef7d2ad2c8543877ba2f17aa4ce46973754b
廖元强 <445639375@qq.com>
2021-02-05 15:00:58 +0800

    记录sql日志

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Events\QueryExecuted;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +14,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
     }
 
     /**
@@ -23,6 +23,55 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->recordQueryLog();
+    }
+
+    /**
+     * 记录数据库查询日志.
+     *
+     * @return void
+     */
+    protected function recordQueryLog()
+    {
+        if (!$this->app['config']->get('app.debug')) {
+            return;
+        }
+
+        \DB::listen(function (QueryExecuted $query) {
+            if ($query->time < $this->app['config']->get('logging.query.slower_than', 0)) {
+                return;
+            }
+
+            $sqlWithPlaceholders = str_replace(['%', '?'], ['%%', '%s'], $query->sql);
+
+            $bindings = $query->connection->prepareBindings($query->bindings);
+            $pdo = $query->connection->getPdo();
+            $realSql = $sqlWithPlaceholders;
+            $duration = $this->formatDuration($query->time / 1000);
+
+            if (count($bindings) > 0) {
+                $realSql = vsprintf($sqlWithPlaceholders, array_map([$pdo, 'quote'], $bindings));
+            }
+
+            logger()->debug(sprintf('[%s] %s | %s: %s', $duration, $realSql, request()->method(), request()->getRequestUri()));
+        });
+    }
+
+    /**
+     * Format duration.
+     *
+     * @param float $seconds
+     *
+     * @return string
+     */
+    private function formatDuration($seconds)
+    {
+        if ($seconds < 0.001) {
+            return round($seconds * 1000000).'μs';
+        } elseif ($seconds < 1) {
+            return round($seconds * 1000, 2).'ms';
+        }
+
+        return round($seconds, 2).'s';
     }
 }
